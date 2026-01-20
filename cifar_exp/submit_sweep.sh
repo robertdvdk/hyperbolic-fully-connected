1dea1c4e3382439dc8397cdca39e4268b9d82d1f
Robert van der Klis <robert.vdklis@protonmail.com>
2026-01-20 21:55:35 +0100

    longer sweep

--- cifar_exp/submit_sweep.sh
+++ cifar_exp/submit_sweep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=cifar_sweep
 #SBATCH --output=./%x_%j.out
-#SBATCH --time=04:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=normal
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=4
