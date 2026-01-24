be5123c461d279d3dc6e17cb24b04b431f53c25e
Robert van der Klis <robert.vdklis@protonmail.com>
2026-01-24 20:45:04 +0100

    weightnorm sweeps

--- FGG-LNN/cifar_exp/submit_sweep_weightnorm.sh
+++ FGG-LNN/cifar_exp/submit_sweep_weightnorm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#SBATCH --job-name=cifar_sweep
+#SBATCH --job-name=cifar_sweep_weightnorm
 #SBATCH --output=./%x_%j.out
-#SBATCH --time=08:00:00
+#SBATCH --time=05:00:00
 #SBATCH --partition=normal
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=4
@@ -19,7 +19,7 @@ echo "GPUs available: $(nvidia-smi -L)"
 
 # Launch 4 agents, one per GPU
 for i in 0 1 2 3; do
-    CUDA_VISIBLE_DEVICES=$i uv run wandb agent "robert-vdklis/hyperbolic-fully-connected-FGG-LNN_cifar_exp/sfcx6hi8" &
+    CUDA_VISIBLE_DEVICES=$i uv run wandb agent "robert-vdklis/hyperbolic-fully-connected-FGG-LNN_cifar_exp/ub0y1fyc" &
     echo "Started agent $i on GPU $i (PID: $!)"
 done
 
