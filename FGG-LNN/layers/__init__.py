17c78f7c1d5585659d8755eb76620b5fc5ce12b4
Robert van der Klis <robert.vdklis@protonmail.com>
2026-01-25 16:20:07 +0100

    runtime

--- FGG-LNN/layers/__init__.py
+++ FGG-LNN/layers/__init__.py
@@ -3,7 +3,7 @@
 from .lorentz import Lorentz
 from .LLinear import LorentzFullyConnected
 from .chen import ChenLinear
-from .poincare import Poincare, Poincare_linear, project
+from .poincare import Poincare, Poincare_linear, project, PoincareActivation
 from .LResNet import lorentz_resnet18
 
-__all__ = ["Lorentz", "LorentzFullyConnected", "ChenLinear", "Poincare", "Poincare_linear", "project", "lorentz_resnet18"]
+__all__ = ["Lorentz", "LorentzFullyConnected", "ChenLinear", "Poincare", "Poincare_linear", "project", "lorentz_resnet18", "PoincareActivation"]
