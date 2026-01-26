fdec6e27b65db67326dbd4566fbda34f1b1b8ebf
Robert van der Klis <robert.vdklis@protonmail.com>
2026-01-26 10:52:27 +0100

    final runs

--- FGG-LNN/cifar_exp/submit_finalruns.sh
+++ FGG-LNN/cifar_exp/submit_finalruns.sh
@@ -19,7 +19,7 @@ echo "GPUs available: $(nvidia-smi -L)"
 
 # Launch 4 agents, one per GPU
 for i in 0 1 2 3; do
-    CUDA_VISIBLE_DEVICES=$i uv run wandb agent "robert-vdklis/hyperbolic-fully-connected-FGG-LNN_cifar_exp/r9nemj61" &
+    CUDA_VISIBLE_DEVICES=$i uv run wandb agent "robert-vdklis/hyperbolic-fully-connected-FGG-LNN_cifar_exp/13qcc6ze" &
     echo "Started agent $i on GPU $i (PID: $!)"
 done
 
